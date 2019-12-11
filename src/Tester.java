145a9dc59ac6dd7574eece57b8f2e5b062085232
Volodyanov Nikita <56234640+besidejke@users.noreply.github.com>
2019-12-11 15:05:35 +0300

    Add files via upload

---
+++ src/Tester.java
@@ -0,0 +1,17 @@
+
+import java.lang.*;
+import java.util.Scanner;
+import com.company.Circle;
+
+public class Tester {
+    public static void main(String[] args) {
+        double r;
+        Circle k1 = new Circle(4.1, 5.1, 7.1, "blue");
+        System.out.println("Длина окружности = " + k1.getLength() + "см\n");
+        Scanner source = new Scanner(System.in);
+        System.out.println("Введите радиус ");
+        r = source.nextDouble();
+        k1.setR(r);
+        System.out.println("\nДлина окружности = " + k1.getLength() + "см");
+    }
+}
